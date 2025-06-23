0be3eb23cdd5598a6aee95d02fc796245e1f1494
ali-chughtai <alichughtai67@gmail.com>
2025-06-23 17:13:31 +0500

    Monday first commit

--- public/images/flags/flag/flagImages.tsx
+++ public/images/flags/flag/flagImages.tsx
@@ -7,6 +7,10 @@ import southKoreaFlag from './kr.png';
 import ukFlag from './gb.png';
 import franceFlag from './fr.png';
 import argentinaFlag from './ar.png';
+import australiaFlag from './australia.png';
+import japanFlag from './japan.png';
+import russiaFlag from './russia.png';
+import chinaFlag from './china.png';
 
 const flags = {
     america: americaFlag,
@@ -17,6 +21,11 @@ const flags = {
     uk: ukFlag,
     france: franceFlag,
     argentina: argentinaFlag,
+    australia: australiaFlag,
+    japan: japanFlag,
+    russia: russiaFlag,
+    china: chinaFlag,
+    
 }
 
 export default flags;
--- src/components/main/profiles.tsx
+++ src/components/main/profiles.tsx
@@ -4,65 +4,124 @@ import flags from "../../../public/images/flags/flag/flagImages";
 import Image from "next/image";
 
 export default function Profiles() {
+
   const profiles = [
     {
       profilePicture: profileImages.koreanProfessor,
-      name: "Professor Tae-joon",
-      nationality: flags.south_korea,
-      degree: "Masters in AeroSpace Engineering",
-      description: "part of our team, guided 100+ students",
+      name: "Ms. Inzish Sajid (UK)",
+      countries: [flags.uk],
+      degree: "Doctoral Researcher, Lecturer",
+      description: ["British Council Projects expertise,", "Commonwealth Scholarships"] 
     },
     {
-      profilePicture: profileImages.americanProfessor,
-      name: "Professor Tim Miller",
-      nationality: flags.america,
-      degree: "PHD in Mathematics",
-      description: "A Leading figure of University of Colarado",
+      profilePicture: profileImages.koreanProfessor,
+      name: "Dr. Adeela (Russia, Japan)",
+      countries: [flags.russia, flags.japan, flags.germany],
+      degree: "Theoretical & Computational Physics research area",
+      description: ["Short term grants in Germany,", "Post Doc hunting trips for Japan and Russia"] 
     },
     {
-      profilePicture: profileImages.germanProfessor,
-      name: "Mr. Bastian",
-      nationality: flags.germany,
-      degree: "Masters in Material Science",
-      description: "Leading research in Nano Techonology and Metallurgy",
+      profilePicture: profileImages.koreanProfessor,
+      name: "Dr. Khizar H. Khan (China)",
+      countries: [flags.china, flags.germany],
+      degree: "Research interest in Energy sector ",
+      description: ["Short term grants in Germany,", "Post Doc hunting for China"] 
     },
     {
-      profilePicture: profileImages.argentinianProfessor,
-      name: "Professor Mateo",
-      nationality: flags.argentina,
-      degree: "Atheletic Director",
-      description:
-        "Experienced coach, with experties in Athletic leadership and Management",
+      profilePicture: profileImages.koreanProfessor,
+      name: "Ms. Shafqat (Italy)",
+      countries: [flags.italy, flags.uk],
+      degree: "Master study routes and opportunities",
+      description: ["Hunting for master studies in England"] 
     },
     {
-      profilePicture: profileImages.frenchProfessor,
-      name: "Dr. Claude",
-      nationality: flags.france,
-      degree: "PHD in Interior Design and Architecture",
-      description: "Took parts in Architectural projects at National Level",
+      profilePicture: profileImages.koreanProfessor,
+      name: "Ms. Amna (Australia)",
+      countries: [flags.australia],
+      degree: "Researcher in Polymer Science and Engineering",
+      description: [, "Doctoratal study route to Australia"] 
     },
     {
-      profilePicture: profileImages.italianProfessor,
-      name: "Ms. Maria",
-      nationality: flags.italy,
-      degree: "Masters in Media Sciences",
-      description: "Leading figure in Media Sciences",
+      profilePicture: profileImages.koreanProfessor,
+      name: "Ms. Aiman (USA)",
+      countries: [flags.america],
+      degree: "Researcher in field of Supercapacitors/Batteries",
+      description: ["Guidelines for admission processors in USA universities"] 
     },
     {
-      profilePicture: profileImages.canadianProfessor,
-      name: "Mr. Dave Williams",
-      nationality: flags.canada,
-      degree: "PHD in Accounting Finance",
-      description: "15+ years of experience in Banking and Taxation sectors",
+      profilePicture: profileImages.koreanProfessor,
+      name: "Ms. Baria (Germany)",
+      countries: [flags.germany],
+      // degree: "Researcher in field of Supercapacitors/Batteries",
+      description: ["Study routes for masters in German Universities "] 
     },
     {
-      profilePicture: profileImages.ukProfessor,
-      name: "Ms. Michelle",
-      nationality: flags.uk,
-      degree: "Professor of History",
-      description: "Preseved the history of the Great Britain",
+      profilePicture: profileImages.koreanProfessor,
+      name: "Ms. Ammara (Germany)",
+      countries: [flags.germany],
+      // degree: "Researcher in field of Supercapacitors/Batteries",
+      description: ["Master study guidelines in German Universities "] 
     },
-  ];
+  ]
+  // const profiles = [
+  //   {
+  //     profilePicture: profileImages.koreanProfessor,
+  //     name: "Professor Tae-joon",
+  //     countries: flags.south_korea,
+  //     degree: "Masters in AeroSpace Engineering",
+  //     description: "part of our team, guided 100+ students",
+  //   },
+  //   {
+  //     profilePicture: profileImages.americanProfessor,
+  //     name: "Professor Tim Miller",
+  //     countries: flags.america,
+  //     degree: "PHD in Mathematics",
+  //     description: "A Leading figure of University of Colarado",
+  //   },
+  //   {
+  //     profilePicture: profileImages.germanProfessor,
+  //     name: "Mr. Bastian",
+  //     countries: flags.germany,
+  //     degree: "Masters in Material Science",
+  //     description: "Leading research in Nano Techonology and Metallurgy",
+  //   },
+  //   {
+  //     profilePicture: profileImages.argentinianProfessor,
+  //     name: "Professor Mateo",
+  //     countries: flags.argentina,
+  //     degree: "Atheletic Director",
+  //     description:
+  //       "Experienced coach, with experties in Athletic leadership and Management",
+  //   },
+  //   {
+  //     profilePicture: profileImages.frenchProfessor,
+  //     name: "Dr. Claude",
+  //     countries: flags.france,
+  //     degree: "PHD in Interior Design and Architecture",
+  //     description: "Took parts in Architectural projects at National Level",
+  //   },
+  //   {
+  //     profilePicture: profileImages.italianProfessor,
+  //     name: "Ms. Maria",
+  //     countries: flags.italy,
+  //     degree: "Masters in Media Sciences",
+  //     description: "Leading figure in Media Sciences",
+  //   },
+  //   {
+  //     profilePicture: profileImages.canadianProfessor,
+  //     name: "Mr. Dave Williams",
+  //     countries: flags.canada,
+  //     degree: "PHD in Accounting Finance",
+  //     description: "15+ years of experience in Banking and Taxation sectors",
+  //   },
+  //   {
+  //     profilePicture: profileImages.ukProfessor,
+  //     name: "Ms. Michelle",
+  //     countries: flags.uk,
+  //     degree: "Professor of History",
+  //     description: "Preseved the history of the Great Britain",
+  //   },
+  // ];
 
   return (
     <div
@@ -120,10 +179,33 @@ export default function Profiles() {
               />
               <h1 className="font-bold text-lg">{profile.name}</h1>
               <h2 className="italic">{profile.degree}</h2>
-              <p className="text-sm text-gray-600">"{profile.description}"</p>
+              
+              {Array.isArray(profile.description) ? (
+                profile.description.map((item, idx) => (
+                  <p key={idx} className="text-sm text-gray-600">{item}</p>
+                ))
+              ) : (
+                <p className="text-sm text-gray-600">{profile.description}</p>
+              )}
+              
               <div className="flex gap-2">
-                <p className="font-semibold">Nationality:</p>
-                <Image src={profile.nationality} alt="" width={40} />
+                <p className="font-semibold">Countries:</p>
+                {Array.isArray(profile.countries) ? (
+                  profile.countries.map((country, idx) => (
+                    <div key={idx} className="border border-gray-300 ">
+                      <Image 
+                        src={country} 
+                        alt="" 
+                        width={40} 
+                        height={24}
+                        className="w-full h-full object-cover"
+                      />
+                    </div>
+                  ))
+                ) : (
+                  <Image src={profile.countries} alt="" width={40} />
+                )}
+                {/* <Image src={profile.countries} alt="" width={40} /> */}
               </div>
             </div>
           );
